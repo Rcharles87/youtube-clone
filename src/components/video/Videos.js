f3dc17fe8784e9041af86feafabd7d1d1fb4d431
rae charles <r_charles87@yahoo.com>
2021-12-06 18:08:44 -0500

    just saving work place

--- src/components/video/Videos.js
+++ src/components/video/Videos.js
@@ -1,14 +1,53 @@
 import { Component } from "react";
 import YouTube from "react-youtube";
 
-class Videos extends Component{
-
-
-    render(){
-        return(
-            <YouTube />
-        )
+class Videos extends Component {
+    constructor(){
+        super()
+        this.state={
+            userInput: ""
+        }
     }
-}
+
+
+
+    handleSearchform(event){
+        event.preventDefault()
+        // this.setState({
+        //     userInput: event.target.value
+        // })
+        console.log(event.target.value)
+    }
+    
+
+    render() {
+        // console.log(this.state.userInput)
+
+      const opts = {
+        height: '390',
+        width: '640',
+        playerVars: {
+          // https://developers.google.com/youtube/player_parameters
+          autoplay: 1,
+        },
+      };
+  
+      return(
+          <div>
+             <form onInput ={this.handleSearchform}>
+                 <input type="text" id="search" name="search"/>
+                 <input type="submit" value="submit"/>
+             </form>
+            <YouTube videoId="2g811Eo7K8U" opts={opts} onReady={this._onReady} />
+
+          </div>
+      ) 
+    }
+  
+    _onReady(event) {
+      // access to player in all event handlers via event.target
+      event.target.pauseVideo();
+    }
+  }
 
 export default Videos
