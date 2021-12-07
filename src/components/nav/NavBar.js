292592b2c6fab8bb2c9cbf3da379b15bf14cab61
rae charles <r_charles87@yahoo.com>
2021-12-07 11:04:01 -0500

    reworked navbar component

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,44 +1,47 @@
 import { Component } from "react";
 import "./NavBar.css"
+import MenuIcon from "@material-ui/icons/Menu";
+import { Search } from "@material-ui/icons";
+import { VideoCall } from "@material-ui/icons";
+import { Apps } from "@material-ui/icons";
+import { Notifications } from "@material-ui/icons";
 
-class Nav extends Component {
-constructor(){
-    super();
-    this.state = {
-        search:""
+
+class NavBar extends Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+
+            <div className="navbar">
+
+                <div className="navbar-left">
+                    <MenuIcon />
+                <img
+                className="navbar-logo"
+                 src="https://upload.wikimedia.org/wikipedia/commons/e/e1/Logo_of_YouTube_%282015-2017%29.svg" 
+                 alt="" 
+                 />
+                </div>
+
+                <div className="navbar-input">
+                 <input placeholder="Search..." type="text" />
+                <Search className="navbar-inputButton"/>
+                </div>
+
+                <div className="navbar-icons">
+                <VideoCall className="navbar-icon"/>
+                <Apps className="navbar-icon"/>
+                <Notifications className="navbar-icon"/>
+                </div>
+
+  
+            </div>
+        )
     }
 }
 
-handleformSubmit=(event)=>{
-    event.preventDefault();
-    
-}
 
-render(){
-    return(
-        <div className="Navbar">
-        <article>
-        <h1>
-       Youtube Clone
-      </h1>
-      </article>
-      <div>
-      <form onSubmit={this.handleformSubmit}>
-       <input
-       type="text" name="name"
-       />
-       <input type="submit" value="Submit"
-       />
-      </form>
-      </div>
-      <aside>
-      <h3>
-          About Us 
-        </h3>
-        </aside>
-      </div>
-    )
-}
-}
-
-export default Nav
+export default NavBar
