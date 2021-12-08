13c12229dd799faa2a870aa88ea35f76073562f9
Laiba Sajid Khan <sajid.laiba378@gmail.com>
2021-12-08 02:57:42 -0500

    finished about-project-description

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,7 +13,17 @@ class About extends Component{
                <h1>About</h1>
               <div className="project-description">
                   <h2>Project Description</h2>
-                  <p>A small team of three people building a working youtube clone from scratch.</p>
+                  <p>A small team of three people building a working youtube clone from scratch. 
+                      A React application was created that allows users to search for YouTube videos using the YouTube API. 
+                      Once the user has entered their desired "search input" a series of videos are displayed for the user to select from. 
+                      Data was retrieved from API and rendered accordingly to display desired videos. 
+                      Additionally, users are able to select a video from a list to watch based on inputted fields within the search bar. 
+                      The project aims to recreate parts of the YouTube application, mimicking YouTube functionality. 
+                      Furthermore, YouTube Clone consists of features such as but not limitng to nav-Bar, home-page, and side-Bar. 
+                      Components have been rendered and displayed through data to present precise information at the area of interest. 
+                      When the user lands upon the home page, a series of "popular videos" are displayed. 
+                      These popular videos have mounted upon initial user arrival.</p>
+                  
               </div>
 
                 <div className="team-bio">
